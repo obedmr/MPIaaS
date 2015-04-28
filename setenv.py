10362344197a6a61f262785362ff9230a937f3b3
Obed N Munoz <obed.n.munoz@gmail.com>
2015-04-27 23:55:04 -0500

    Single function for containers setup
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

--- setenv.py
+++ setenv.py
@@ -14,25 +14,38 @@ def execute(command):
 
 def vagrant_up():
     """ Vagrant startup """
-    execute("wget https://get.docker.com/builds/Linux/x86_64/docker-1.5.0")
-    execute("chmod 755 ./docker-1.5.0")
-    execute("git clone https://github.com/coreos/coreos-vagrant.git")
+    if not os.path.exists("./docker-1.5.0"):
+        execute("wget https://get.docker.com/builds/Linux/x86_64/docker-1.5.0")
+        execute("chmod 755 ./docker-1.5.0")
+    if not os.path.exists("coreos-vagrant"):
+        execute("git clone https://github.com/coreos/coreos-vagrant.git")
     execute("cp user-data coreos-vagrant/")
     execute("cp config.rb coreos-vagrant/")
     execute("cd coreos-vagrant/ ; vagrant up")
 
 
-def create_containers(virtual=True, clients_count=3, clients=[]):
-    """ Creates Linux Containers on Clients """
-    if virtual:
-        for client in range(0, clients_count):
-            clients.append({"host": "localhost", "port": DOCKER_TCP + client})
-            
-        for client in clients:
-            client_number = client["port"] - DOCKER_TCP
-            print("Creating Containers in Client #" + str(client_number))
-            os.environ['DOCKER_HOST'] = "tcp://localhost:%d" % client["port"]
-            execute("./docker-1.5.0 pull ubuntu")
+def set_containers(host, port):
+    """ Creates Linux Containers on Client """
+
+    client_number = port - DOCKER_TCP
+    print("Creating Containers in Client #" + str(client_number))
+    os.environ['DOCKER_HOST'] = "tcp://%s:%d" % (host, port)
+     
+    if not os.path.exists('./.tmp'):
+        execute("mkdir .tmp")
+        execute("./docker-1.5.0 build -t mpiaas/ubuntu dockerfiles/ubuntu")
+        execute("./docker-1.5.0 save -o ./.tmp/mpiaas_ubuntu mpiaas/ubuntu")
+        execute("./docker-1.5.0 build -t mpiaas/fedora dockerfiles/fedora")
+        execute("./docker-1.5.0 save -o ./.tmp/mpiaas_fedora mpiaas/fedora")
+        execute("./docker-1.5.0 build -t mpiaas/archlinux dockerfiles/archlinux")
+        execute("./docker-1.5.0 save -o ./.tmp/mpiaas_archlinux mpiaas/archlinux")
+        #execute("./docker-1.5.0 build -t mpiaas/centos dockerfiles/centos")
+        #execute("./docker-1.5.0 save -o ./.tmp/mpiaas_centos mpiaas/centos")
+    else:
+        execute("./docker-1.5.0 load -i ./.tmp/mpiaas_ubuntu")
+        execute("./docker-1.5.0 load -i ./.tmp/mpiaas_fedora")
+        execute("./docker-1.5.0 load -i ./.tmp/mpiaas_archlinux")
+        #execute("./docker-1.5.0 load -i ./.tmp/mpiaas_centos")
 
 
 if __name__ == "__main__":
@@ -43,5 +56,8 @@ if __name__ == "__main__":
 
     if virtual:
         vagrant_up()
-        
-    create_containers()
+
+    if virtual:
+        clients_count = 3
+        for client in range(0, clients_count):
+            set_containers("localhost", DOCKER_TCP + client)
