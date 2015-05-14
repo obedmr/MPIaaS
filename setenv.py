e18253ea24035c6cdab06aabe0eb9d7bb889db67
Obed N Munoz <obed.n.munoz@gmail.com>
2015-05-14 01:59:17 -0500

    Generic docker building
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

--- setenv.py
+++ setenv.py
@@ -6,7 +6,6 @@ import json
 
 DOCKER_TCP = 2375
 
-
 def execute(command):
     """ Bash command helper """
     return subprocess.call(command, shell=True)
@@ -27,26 +26,25 @@ def vagrant_up():
 def set_containers(host, port):
     """ Creates Linux Containers on Client """
 
-    client_number = port - DOCKER_TCP
+    client_number = port - DOCKER_TCP + 1
     print("Creating Containers in Client #" + str(client_number))
-    os.environ['DOCKER_HOST'] = "tcp://%s:%d" % (host, port)
-     
-    if not os.path.exists('./.tmp'):
-        execute("mkdir .tmp")
-        execute("./docker-1.5.0 build -t mpiaas/ubuntu dockerfiles/ubuntu")
-        execute("./docker-1.5.0 save -o ./.tmp/mpiaas_ubuntu mpiaas/ubuntu")
-        execute("./docker-1.5.0 build -t mpiaas/fedora dockerfiles/fedora")
-        execute("./docker-1.5.0 save -o ./.tmp/mpiaas_fedora mpiaas/fedora")
-        execute("./docker-1.5.0 build -t mpiaas/archlinux dockerfiles/archlinux")
-        execute("./docker-1.5.0 save -o ./.tmp/mpiaas_archlinux mpiaas/archlinux")
-        #execute("./docker-1.5.0 build -t mpiaas/centos dockerfiles/centos")
-        #execute("./docker-1.5.0 save -o ./.tmp/mpiaas_centos mpiaas/centos")
-    else:
-        execute("./docker-1.5.0 load -i ./.tmp/mpiaas_ubuntu")
-        execute("./docker-1.5.0 load -i ./.tmp/mpiaas_fedora")
-        execute("./docker-1.5.0 load -i ./.tmp/mpiaas_archlinux")
-        #execute("./docker-1.5.0 load -i ./.tmp/mpiaas_centos")
+    base_url = "tcp://%s:%d" % (host, port)
+    os.environ['DOCKER_HOST'] = base_url
+    images = ['ubuntu', 'archlinux', 'fedora']
 
+    for image in images:
+        if (not os.path.exists('.tmp_mpiaas_%s' % image )):
+            execute("./docker-1.5.0 build -t mpiaas/%s dockerfiles/%s"
+                    % ( image, image ) )
+            execute("./docker-1.5.0 save -o .tmp_mpiaas_%s mpiaas/%s"
+                    % ( image, image ) )
+        else:
+            execute("./docker-1.5.0 load -i .tmp_mpiaas_%s"
+                    % image )
+
+
+def main():
+    pass
 
 if __name__ == "__main__":
 
@@ -56,8 +54,6 @@ if __name__ == "__main__":
 
     if virtual:
         vagrant_up()
-
-    if virtual:
         clients_count = 3
         for client in range(0, clients_count):
             set_containers("localhost", DOCKER_TCP + client)
